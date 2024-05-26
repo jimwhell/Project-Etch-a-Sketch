e97213983530c55a93462361cda4803277fe666a
Jimwell <jimwelllmanalo@gmail.com>
2024-05-26 17:14:27 +0800

    Implemented functionality to the program.

--- script.js
+++ script.js
@@ -1,59 +1,67 @@
 const containerDiv = document.getElementById("container");
+const buttonContainer = document.getElementById("button-container");
+const toggleNumSquaresButton = document.createElement("button");
+const resetButton = document.createElement("button");
+const containerWidthHeight = 960;
+let defaultSquareSize = 16; 
+let numOfSquares = 960 / defaultSquareSize;
 
-const firstDiv = document.createElement("div");
-firstDiv.setAttribute("style", "width: 16px; height: 16px;");
-firstDiv.classList.add("firstDiv");
+function createSquares() 
+{
+    const numSquaresX = Math.floor(containerWidthHeight / defaultSquareSize);
+    const numSquaresY = Math.floor(containerWidthHeight / defaultSquareSize);
+    const totalSquares = numSquaresX * numSquaresY;
 
-const secondDiv = document.createElement("div");
-secondDiv.setAttribute("style", "width: 16px; height: 16px;");
-secondDiv.classList.add("secondDiv");
+    for (let i = 0; i < (defaultSquareSize ** 2); i++) {
+        const square = document.createElement("div");
+        square.style.width = numOfSquares + "px";
+        square.style.height = numOfSquares + "px";
+        square.style.backgroundColor = "#fff";
+        containerDiv.appendChild(square);
+        square.addEventListener("mouseover", (e) => {
+            const randomColor = getRandomColor();
+            console.log(randomColor);
+            e.target.style.backgroundColor = randomColor;
+        });
+    }
+}
 
-
-const thirdDiv = document.createElement("div");
-thirdDiv.setAttribute("style", "width: 16px; height: 16px;");
-thirdDiv.classList.add("thirdDiv");
-
-
-const fourthDiv = document.createElement("div");
-fourthDiv.setAttribute("style", "width: 16px; height: 16px;");
-fourthDiv.classList.add("fourthDiv");
-
-
-const fifthDiv = document.createElement("div");
-fifthDiv.setAttribute("style", "width: 16px; height: 16px;");
-fifthDiv.classList.add("fifthDiv");
-
-containerDiv.appendChild(firstDiv);
-containerDiv.appendChild(secondDiv);
-containerDiv.appendChild(thirdDiv);
-containerDiv.appendChild(fourthDiv);
-containerDiv.appendChild(fifthDiv);
-
-const divs = document.querySelectorAll("div");
-
-divs.forEach((div) => {
-    div.addEventListener("mouseover", () => {
-        switch (div.className) {
-            case "firstDiv":
-                div.style.backgroundColor = "blue";
-                break;
-            case "secondDiv":
-                div.style.backgroundColor = "red";
-                break;
-            case "thirdDiv":
-                div.style.backgroundColor = "orange";
-                break;
-            case "fourthDiv":
-                div.style.backgroundColor = "pink";
-                break;
-            case "fifthDiv":
-                div.style.backgroundColor = "yellow";
-                break;
-            default:
-                break;
-        };
+function toggleNumOfSquares() 
+{
+    toggleNumSquaresButton.classList.add("button");
+    toggleNumSquaresButton.textContent = "Change Number of Squares Per Side"; 
+    toggleNumSquaresButton.addEventListener("click", () => {
+        defaultSquareSize = parseInt(prompt("Enter the number of squares per side: "));
+        numOfSquares = 960 / defaultSquareSize;
+        containerDiv.innerHTML = ""; 
+        createSquares(); 
     });
-});
+    buttonContainer.appendChild(toggleNumSquaresButton); 
+}
 
+function resetCanvas()
+{
+    resetButton.classList.add("button");
+    resetButton.textContent = "Reset Canvas";
+    resetButton.addEventListener("click", () => {
+        containerDiv.innerHTML = "";
+        createSquares();
+    });
+    buttonContainer.appendChild(resetButton);
+}
 
+function getRandomColor() 
+{
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    const color = `rgb(${red},${green},${blue})`;
+
+    return color;
+}
+
+createSquares();
+toggleNumOfSquares();
+resetCanvas();
 
